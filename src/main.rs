0671f8f104518f62421fe8c9813404d9f4658ebc
crushedflea8060 <finneganadair@gmail.com>
2026-07-19 12:27:44 -0400

    Add additional system functionality to visual apt.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 //VAPT (visual apt).
-use iced::widget::{button, column, text,  text_input, center_x};
+use iced::widget::{button, column, text,  text_input, center_x, scrollable};
 use std::process::Command;
 use iced::{Element, Alignment, Fill, Size};
 pub fn main() -> iced::Result {
@@ -7,7 +7,7 @@ pub fn main() -> iced::Result {
         .title(Installer::title)
         // We set the window config here.
         .window(iced::window::Settings {
-            size: Size::new(250.0, 350.0),
+            size: Size::new(400.0, 670.0),
             ..Default::default()
         })
         
@@ -21,11 +21,15 @@ struct Installer {
 }
 
 #[derive(Debug, Clone)]
-enum CMD {
+enum CMD { // was named command, but had to be changed because of a conflict with the command module in std::process.
     Remove,
     Install,
     PackageChange(String),
     PurgeUnused,
+    Search,
+    Update,
+    Upgrade,
+    List,
 }
 
 impl Installer {
@@ -35,32 +39,45 @@ impl Installer {
     }
 
 
-    fn update(&mut self, message: CMD) {
+    fn update(&mut self, message: CMD) { // we are using our enum as a sort of control center and outputting the results of the functions into the log.
         match message {
             CMD::Install => self.log = install(self.package.clone()),
             CMD::Remove => self.log = remove(self.package.clone()),
             CMD::PackageChange(new_package) => self.package = new_package,
             CMD::PurgeUnused => self.log = purge(),
+            CMD::Search => self.log = search(self.package.clone()),
+            CMD::Update => self.log = update(),
+            CMD::Upgrade => self.log = upgrade(),
+            CMD::List => self.log = list(),
         }
     }
 
-    fn view(&self) -> Element<'_, CMD> {
-        center_x(column![
-            text("Search for Debian packages at https://packages.debian.org/nl/"),
+    fn view(&self) -> Element<'_, CMD> { //the view function is essentially the layout.
+        center_x(
+            scrollable(column![
+            text("APT Package Search: packages.debian.org/nl/"),
             text(format!("Package Name: {}", &self.package)),
             text_input("", &self.package).on_input(CMD::PackageChange),
             button("Install").on_press(CMD::Install),
             button("Remove").on_press(CMD::Remove),
+            button("Search for Packages").on_press(CMD::Search),
             button("Purge Unused Dependencies").on_press(CMD::PurgeUnused),
-            text(format!("Install Log: {}", &self.log)),
+            text("Buttons below this line will ignore your input, be mindful of what you click."),
+            button("Upgrade System").on_press(CMD::Upgrade),
+            button("Update System").on_press(CMD::Update),
+            button("Generate a txt file of installed packages").on_press(CMD::List),
+            text("The program may become unresponsive while running commands, please be patient. Do not close the program as it could cause issues with your system."),
+            text(format!("Output Log: {}", &self.log)),
         ].spacing(20)
             .width(Fill)                       // Forces the column to span full width
             .align_x(Alignment::Center))
+            .height(Fill)
+        )
             .into()
     }
 }
 
-fn install(package: String) -> String
+fn install(package: String) -> String //we're running apt install and telling the user if the install was successful.
 {
     println!("installing {}", package.clone());
     let output = Command::new("sudo")
@@ -82,7 +99,8 @@ fn install(package: String) -> String
         return format!("APT failed with exit code: {:?}", output.status.code())
     }
 }
-fn remove(package: String) -> String
+fn remove(package: String) -> String // we're essentially just running apt remove. I didn't include purge as there is already a separate button. 
+// we are not purging automatically because it could break dependencies for the user.
 {
     println!("removing {}", package.clone());
     let output = Command::new("sudo")
@@ -104,7 +122,7 @@ fn remove(package: String) -> String
         return format!("APT failed with exit code: {:?}", output.status.code())
     }
 }
-fn purge() -> String
+fn purge() -> String // we're running apt autoremove --purge -y to remove unused packages
 {
     println!("purging unused packages");
     let output = Command::new("sudo")
@@ -118,5 +136,73 @@ fn purge() -> String
         return format!("Packages purged successfully!")
     } else {
         return format!("APT failed with exit code: {:?}", output.status.code())
+    }
+}
+
+fn search(package: String) -> String {
+    println!("searching for {}", package.clone());
+    let output = Command::new("apt-cache")
+        .arg("search")
+        .arg("--names-only")
+        .arg(package)
+        .output()
+        .expect("Failed to execute apt command");
+    println!("searching for packages has completed");
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let string_stdout = stdout.to_string();
+        let mut package_arr = string_stdout.split("\n").map(|pack| pack.split(" - ").nth(0).unwrap_or("").trim()).collect::<Vec<&str>>();
+        package_arr.sort_by_key(|s| s.len());
+        let package_list = package_arr.join("\n");
+        return format!("Search results:\n{}", package_list);
+    } else {
+        return format!("APT failed with exit code: {:?}", output.status.code())
+    }
+}
+
+fn upgrade() -> String {
+    println!("upgrading system");
+    let output = Command::new("sudo")
+        .arg("apt")
+        .arg("upgrade")
+        .arg("-y")
+        .output()
+        .expect("Failed to execute apt command");
+    if output.status.success() {
+        return format!("System upgraded successfully!")
+    } else {
+        return format!("APT failed with exit code: {:?}", output.status.code())
+    }
+}
+
+fn update() -> String {
+    println!("updating system");
+    let output = Command::new("sudo")
+        .arg("apt")
+        .arg("update")
+        .output()
+        .expect("Failed to execute apt command");
+    if output.status.success() {
+        return format!("System updated successfully!")
+    } else {
+        return format!("APT failed with exit code: {:?}", output.status.code())
+    }
+}
+
+fn list() -> String {
+    println!("generating package list");
+    let output = Command::new("apt")
+        .arg("list")
+        .arg("--installed")
+        .output()
+        .expect("Failed to execute apt command");
+    if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        let string_stdout = stdout.to_string();
+        let package_count = string_stdout.as_bytes().iter().filter(|&&c| c == b'\n').count();
+        std::fs::write("installed_packages.txt", string_stdout).expect("Unable to write file");
+        return format!("Package list generated successfully! Check installed_packages.txt in the current directory. Total packages: {}", package_count)
+    } else {
+        return format!("apt failed with exit code: {:?}", output.status.code())
     }
 }
